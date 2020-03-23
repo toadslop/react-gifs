b8fb0819b8b8c3157e88a80dabf3d6b84aa10db1
Brian Heise <bnheise@gmail.com>
2020-03-23 14:22:13 +0900

    made search bar and gif components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import Gif from './Gif';
 
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div>
-        <div className="left-scene"></div>
-        <div className="right-scene"></div>
+        <div className = "left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif />
+          </div>
+        </div>
+        <div className = "right-scene" ></div>
       </div>
     )
   }
 }
+
+export default App;
