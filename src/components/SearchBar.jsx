5b3036969cd67cff2f8061001ac5b4a907a37602
Brian Heise <bnheise@gmail.com>
2020-03-23 22:25:35 +0900

    added toggling class to search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isFocused: false,
+      blurredClasses: "form-control form-search",
+      focusedClasses: "form-control form-search focused"
+    };
+  }
+ 
   handleChange = (event) => {
     this.props.search(event.target.value);
   }
 
+  handleFocus = () => {
+    this.setState({ isFocused: true });
+  }
+
+  handleBlur = () => {
+    this.setState({ isFocused: false });
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search" onChange={this.handleChange} />
+      <input type="text" className={this.state.isFocused ? this.state.focusedClasses : this.state.blurredClasses} onFocus={this.handleFocus} onBlur={this.handleBlur} onChange={this.handleChange} />
     );
   }
 }
